ea02443e4f74d1f4c9bee58c968280b1144612ee
zhangxinyi1996 <innerpeacezxy@gmail.com>
2026-07-25 07:35:13 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,11 @@
-const CACHE="move-planner-animal-v3-2";
+const CACHE="move-planner-svg-v2-0";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
-
-self.addEventListener("install",event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-});
-
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",event=>{
-  const req=event.request;
-  if(req.mode==="navigate"){
-    event.respondWith(
-      fetch(req,{cache:"no-store"})
-        .then(res=>{
-          const copy=res.clone();
-          caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
-          return res;
-        })
-        .catch(()=>caches.match("./index.html"))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(req).then(cached=>{
-      return cached || fetch(req).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(cache=>cache.put(req,copy));
-        return res;
-      });
-    })
-  );
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+ if(e.request.mode==="navigate"){
+  e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",x));return r}).catch(()=>caches.match("./index.html")));
+  return;
+ }
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
